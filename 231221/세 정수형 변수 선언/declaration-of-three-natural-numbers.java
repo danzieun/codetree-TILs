0925d6357e02f271ec583843525a12f7fd37c607
이지은 <111346270+danzieun@users.noreply.github.com>
2023-12-21 16:59:03 +0900

    Title: 세 정수형 변수 선언, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231221/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7, b = 23;
+        int c = a + b;
+
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
